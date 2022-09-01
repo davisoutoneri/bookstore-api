d6fc101962bf5ca5fb7a491fc4077b0f7a1ffa5b
davisoutoneri <davisouto.dev@gmail.com>
2022-09-01 19:10:54 -0300

    commit inicial

---
+++ src/main/java/com/davisoutoneri/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.davisoutoneri.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
