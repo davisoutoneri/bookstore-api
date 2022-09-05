516415a4103fe2cfcfc51181b16cfdb41064d768
davisoutoneri <davisouto.dev@gmail.com>
2022-09-05 18:34:45 -0300

    Aula 11: Exceção personalizada para objeto não encontrado

--- src/main/java/com/davisoutoneri/bookstore/service/CategoriaService.java
+++ src/main/java/com/davisoutoneri/bookstore/service/CategoriaService.java
@@ -6,16 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.davisoutoneri.bookstore.domain.Categoria;
+import com.davisoutoneri.bookstore.exceptions.ObjectNotFoundException;
 import com.davisoutoneri.bookstore.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
