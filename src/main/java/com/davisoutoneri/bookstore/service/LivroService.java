a7a5dbf1350f155903283c5e02de70b4dfe40b5a
davisoutoneri <davisouto.dev@gmail.com>
2022-09-10 18:24:36 -0300

    Aula 17: GET para Livro por ID

---
+++ src/main/java/com/davisoutoneri/bookstore/service/LivroService.java
@@ -0,0 +1,23 @@
+package com.davisoutoneri.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.davisoutoneri.bookstore.domain.Livro;
+import com.davisoutoneri.bookstore.repositories.LivroRepository;
+import com.davisoutoneri.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				("Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName())));
+	}
+}
